138e623ae1328584919e0d1779990f8763b458e4
KovaDev <kova.dev@outlook.com>
2015-05-22 20:25:44 +0000

    Adds user controller test

--- Gemfile
+++ Gemfile
@@ -32,5 +32,6 @@ group :test do
 	gem "rspec-rails"
 	gem "factory_girl_rails"
 	gem 'ffaker'	
+	gem "shoulda-matchers"
 end	
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe User, type: :model do
 	it { expect(subject).to respond_to(:email) }
 	it { expect(subject).to respond_to(:password) }
 	it { expect(subject).to respond_to(:password_confirmation) }
+
+	it { expect(subject).to validate_presence_of(:email) }
+	it { expect(subject).to validate_uniqueness_of(:email) }
+	it { expect(subject).to validate_confirmation_of(:password) }
+	it { expect(subject).to allow_value('example@domain.com').for(:email) }
+
+
 	it { expect(subject).to be_valid }
 
 end
