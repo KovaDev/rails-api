7acccd5a8d2931e2156e5b2e73759043560f41f7
KovaDev <kova.dev@outlook.com>
2015-05-22 23:49:30 +0000

    Adds destroy session action

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -41,4 +41,16 @@ RSpec.describe Api::V1::SessionsController, type: :controller do
 
 	end	
 
+	describe "DELETE #destroy" do
+
+		before(:each) do
+			@user = FactoryGirl.create :user
+			sign_in @user # , store: false
+			delete :destroy, id: @user.auth_token
+		end	
+
+		it { expect(response).to have_http_status 204 }
+
+	end	
+
 end
