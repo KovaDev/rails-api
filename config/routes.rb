863ecbff375fe2fa2ac20c7f87019e8f94cc0dda
KovaDev <kova.dev@outlook.com>
2015-05-22 21:01:14 +0000

    Adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   
-  devise_for :users
   require 'api_constraints'
   
   namespace :api, defaults: { format: :json } do
 
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
 
-      resources :users, :only => [:show]
+      resources :users, :only => [:show, :create]
 
     end  
 
   end  
+
+  devise_for :users
 
 end
